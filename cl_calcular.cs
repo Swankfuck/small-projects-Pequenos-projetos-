cf458c0cbde51bd938f6a669b3d3627d48095fa4
Vitor Brussolo Zerbato <70340981+Swankfuck@users.noreply.github.com>
2020-10-12 22:47:28 -0300

    Add files via upload

---
+++ cl_calcular.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProjeto2
+{
+    class cl_calcular
+    {
+        private int porcentagem = 5;
+        private int porcentagem2 = 5;
+
+        public int CalcularPrecoFinal(int preco_inicial, int tipo_produto)
+        {
+            int preco_final = 0;
+
+            switch (tipo_produto)
+            {
+                case 1:
+                    preco_final = preco_inicial + (preco_final * porcentagem / 100);
+                    break;
+
+                case 2:
+                    preco_final = preco_inicial / (preco_final * porcentagem2 / 100);
+                    break;
+
+                
+            }
+            return preco_final;
+        }
+
+
+    }
+}
